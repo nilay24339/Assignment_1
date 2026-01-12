15ef227b359e3cb0daab9dea7298d3910413e788
nilay24339 <nilaymishra24339@gmail.com>
2026-01-12 16:33:27 +0530

    modify task 2

--- Task2.py
+++ Task2.py
@@ -1,4 +1,9 @@
+# Collecting user data
 first_name=input("Enter your name: ")
 last_name=input("Enter your last name: ")
+
+# Joining strings (Concatenation)
 full_name = first_name +" "+ last_name
+
+# Output
 print("Hello," + full_name +"! Welcome to the Python Program")
