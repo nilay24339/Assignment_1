1f71753c3dcada3ff3c68e076326aa5c8a44432d
nilay24339 <nilaymishra24339@gmail.com>
2026-01-12 16:21:37 +0530

    Python Assignment

---
+++ Task1.py
@@ -0,0 +1,13 @@
+
+A = float(input("Enter the first number: "))
+B = float(input("Enter the second number: "))
+
+Addition = A + B
+Subtraction = A - B
+Multiplication = A * B
+Division = A / B
+
+print("Addition: " ,Addition)
+print("Subtraction: " ,Subtraction)
+print("Multiplication: " ,Multiplication)
+print("Division: " , Division)
